d1593363f8ad62f65cb4074b8003d7f86b804695
sriyalj <sriyal.jayasinghe@gmail.com>
2020-05-12 21:05:44 +0300

    Add New Author Feature Added

---
+++ src/DBConn/AuthorDBConn.java
@@ -0,0 +1,63 @@
+package DBConn;
+
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+import Entity.Author;
+
+public class AuthorDBConn {
+	
+	private String url = "jdbc:mysql://localhost:3306/BookStore?useSSL=false&serverTimezone=UTC";
+    private String user = "root";
+    private String password = "Sriyal.meh21";
+    private Connection con = null;
+    
+    public AuthorDBConn () {
+    	
+    }
+    
+    public boolean addAuthor(Author obj) throws SQLException{
+    	
+    	boolean success = false;
+    	
+    	String insertQuery = " insert into Authors (Fst_Name, Mdle_Name, Lst_Name, Origin_Country)"
+    	        + " values (?, ?, ?, ?)";
+    	
+    	try {
+    	    con = DriverManager.getConnection(url, user, password);
+    		PreparedStatement preparedStmt = con.prepareStatement(insertQuery);
+            preparedStmt.setString (1, obj.getFstName());
+            preparedStmt.setString (2, obj.getMiddleName());
+            preparedStmt.setString (3, obj.getLastName());
+            preparedStmt.setString (1, obj.getOriginCountry());
+            success = preparedStmt.execute();
+            //System.out.println("Inserting Done");
+    	}
+    	finally {
+    		if (con != null) {
+    			try { 
+    				con.close(); 
+    			} 
+    			catch (SQLException ex) {
+    				Logger lgr = Logger.getLogger(AuthorDBConn.class.getName());
+    	            lgr.log(Level.SEVERE, ex.getMessage(), ex);
+    			}
+    		}
+    	}
+    	return success;
+    }
+
+}
--- src/authors/AddAuthor.java
+++ src/authors/AddAuthor.java
@@ -2,14 +2,24 @@ package authors;
 
 
 import java.io.BufferedReader;
+import java.io.ByteArrayInputStream;
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.ObjectInputStream;
+import java.sql.SQLException;
+
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import DBConn.AuthorDBConn;
+import Entity.Author;
+
 import java.util.*;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 /**
  * Servlet implementation class View
@@ -17,6 +27,7 @@ import java.util.*;
 @WebServlet("/authors/addBook")
 public class AddAuthor extends HttpServlet {
 	private static final long serialVersionUID = 1L;
+	private ArrayList <String> res = new ArrayList <> ();
        
     /**
      * @see HttpServlet#HttpServlet()
@@ -31,7 +42,8 @@ public class AddAuthor extends HttpServlet {
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
-		       
+		System.out.println ("Add Author Servlet doGet Called");
+		response.getWriter().println("Add Author Servlet doGet Response");      
 	}
 
 	/**
@@ -39,27 +51,48 @@ public class AddAuthor extends HttpServlet {
 	 */
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
+		byte[] message = null;
 		
-		if (request.getParameter("FirstNumber") == null && request.getParameter("SecondNumber") == null && request.getParameter("Operation") == null ) {
-			String data = "";   
-			StringBuilder builder = new StringBuilder();
-			BufferedReader reader = request.getReader();
-			String line;
-			while ((line = reader.readLine()) != null) {
-				builder.append(line);
+		System.out.println ("Add Author Servlet doPost Called");
+		
+		request.setCharacterEncoding("UTF-8");
+	    int contentLen = request.getContentLength();
+		InputStream is = request.getInputStream();
+		if (contentLen > 0) {
+			int readLen = 0;
+			int readLengthThisTime = 0;
+			message = new byte[contentLen];
+			try {
+				while (readLen != contentLen) {
+					readLengthThisTime = is.read(message, readLen, contentLen - readLen);
+					if (readLengthThisTime == -1) {
+						break;
+					}
+					readLen += readLengthThisTime;
+				}
+				ByteArrayInputStream in = new ByteArrayInputStream(message);
+			    ObjectInputStream ois = new ObjectInputStream(in);
+			    Author authorObj = (Author)ois.readObject();
+			    AuthorDBConn dbCon =  new AuthorDBConn ();
+			    dbCon.addAuthor(authorObj);
+			    response.getWriter().println("Server Response Code " + Integer.toString(HttpServletResponse.SC_OK));       		
+	            response.getWriter().println("New Author Added Succesfully");
+			    
+			} catch (IOException | ClassNotFoundException ex ) {
+				
+				Logger lgr = Logger.getLogger(AuthorDBConn.class.getName());
+	            lgr.log(Level.SEVERE, ex.getMessage(), ex);
+	            
+	            response.getWriter().println("Server Response Code " + Integer.toString(HttpServletResponse.SC_INTERNAL_SERVER_ERROR));       		
+	            response.getWriter().println("Service falied to read the data sent");
+			} catch (SQLException ex ) {
+				Logger lgr = Logger.getLogger(AuthorDBConn.class.getName());
+	            lgr.log(Level.SEVERE, ex.getMessage(), ex);
+	            
+	            response.getWriter().println("Server Response Code " + Integer.toString(HttpServletResponse.SC_INTERNAL_SERVER_ERROR));       		
+	            response.getWriter().println("Service falied write the data to the DB");
 			}
-			data = builder.toString();					
-			/*
-			Controller control = new Controller (data);
-			ArrayList <String> ans = control.doPost();
-	        
-	        for (int cnt = 0; cnt < ans.size(); cnt++) {
-	        	response.getWriter().println(ans.get(cnt));
-	         }
-	         */
-		}
-		else {
-			doGet(request, response);		
-		}			
+		}		
+		
 	}
 }
